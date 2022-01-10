a60c53e7a52cc3bd547dc58470ca49a8e4ff430e
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2022-01-10 20:06:19 +0530

    desktop public path vue config

--- frontend-desktop/vue.config.js
+++ frontend-desktop/vue.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 module.exports = {
+	publicPath: process.env.NODE_ENV === 'production' ? '/desktop/' : '/',
+
 	outputDir: path.resolve(__dirname, '../prod/desktop'),
 
 	devServer: {
