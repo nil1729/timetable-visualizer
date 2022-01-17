cda5bc7b067d74dcb51e84faaa8ed258998e14a2
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2022-01-18 02:21:54 +0530

    new mailer

--- src/routes/feedback.js
+++ src/routes/feedback.js
@@ -1,41 +1,56 @@
-const express = require("express");
+const { response } = require('express');
+const express = require('express');
 const router = express.Router();
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const fs = require("fs");
+const fs = require('fs');
+const nodemailer = require('nodemailer');
 
-router.post("/to-me", async (req, res) => {
-  try {
-    const output = `
-            <p>You have a new Feedback for Timetable Companion</p>
-            <h3>Feedback Details</h3>
-            <ul>
-                <li>Message: ${req.body.feedback_message} </li>
-                <li>Rating: ${req.body.rating} </li>
-                <li>Date: ${new Date().toLocaleString("en-IN")} </li>
-            </ul>
-        `;
+router.post('/to-me', async (req, res) => {
+	try {
+		const output = `
+			<p>You have a new Feedback for Timetable Companion</p>
+			<h3>Feedback Details</h3>
+			<ul>
+				<li>Message: ${req.body.feedback_message} </li>
+				<li>Rating: ${req.body.rating} </li>
+				<li>Date: ${new Date().toLocaleString('en-IN')} </li>
+			</ul>
+		`;
 
-    const msg = {
-      to:
-        process.env.NODE_ENV === "production"
-          ? process.env.PROD_RECEIVER_MAIL
-          : process.env.DEV_RECEIVER_MAIL,
-      from: `Server Admin <${process.env.SENDGRID_SENDER_MAIL}>`,
-      subject: "Feedback from Timetable Companion",
-      html: output,
-    };
-    await sgMail.send(msg);
+		let transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: process.env.SENDER_MAIL,
+				pass: process.env.SENDER_MAIL_PASSWORD,
+			},
+		});
 
-    // Save locally on Server
-    fs.appendFileSync("feedback.txt", output);
-    res.status(200).json({
-      success: true,
-      message: "Feedback sent successfully",
-    });
-  } catch (e) {
-    console.log(e);
-  }
+		let mailOptions = {
+			from: `Timetable Companion Server Admin <${process.env.SENDER_MAIL}>`,
+			to:
+				process.env.NODE_ENV === 'production'
+					? [process.env.DEV_RECEIVER_MAIL, process.env.PROD_RECEIVER_MAIL]
+					: process.env.DEV_RECEIVER_MAIL,
+			subject: 'New Feedback from Timetable Companion',
+			html: output,
+		};
+
+		const response = await transporter.sendMail(mailOptions);
+
+		// Save locally on Server
+		fs.appendFileSync('feedback.txt', output);
+		const mailLogsArr = fs.existsSync(process.env.MAIL_LOG_FILE)
+			? JSON.parse(fs.readFileSync(process.env.MAIL_LOG_FILE).toString())
+			: [];
+		mailLogsArr.push(response);
+		fs.writeFileSync(process.env.MAIL_LOG_FILE, JSON.stringify(mailLogsArr));
+
+		res.status(200).json({
+			success: true,
+			message: 'Feedback sent successfully',
+		});
+	} catch (e) {
+		console.log(e);
+	}
 });
 
 module.exports = router;
