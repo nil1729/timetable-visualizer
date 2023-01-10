a26bb90701ed51c47c9f1534089ac75fa697c70e
NILANJAN DEB <nilanjan172nsvian@gmail.com>
2023-01-10 14:05:52 +0530

    Update timetable for Sem II

--- frontend-desktop/src/components/pages/Search.vue
+++ frontend-desktop/src/components/pages/Search.vue
@@ -27,10 +27,7 @@
       <v-container>
         <v-row justify="center">
           <v-col v-for="n in 6" :key="n" cols="4">
-            <v-skeleton-loader
-              v-bind="skeletonAttrs"
-              type="article, actions"
-            ></v-skeleton-loader>
+            <v-skeleton-loader v-bind="skeletonAttrs" type="article, actions"></v-skeleton-loader>
           </v-col>
         </v-row>
       </v-container>
@@ -38,12 +35,7 @@
     <div v-else-if="!searching && searchResults.length === 0">
       <v-container>
         <v-row justify="center">
-          <v-img
-            contain
-            max-height="450"
-            max-width="450"
-            src="@/assets/no-data.jpg"
-          ></v-img>
+          <v-img contain max-height="450" max-width="450" src="@/assets/no-data.jpg"></v-img>
         </v-row>
       </v-container>
     </div>
@@ -55,9 +47,7 @@
               <div class="primary--text text-body-1">
                 Total {{ pagination.totalCourses }} Courses Found
                 <br />
-                <small class="warning--text"
-                  >Last Updated on: January 14<sup>th</sup>, 2022</small
-                >
+                <small class="warning--text">Last Updated on: January 10<sup>th</sup>, 2023</small>
               </div>
             </v-col>
             <v-col cols="8">
@@ -76,16 +66,11 @@
       </div>
       <v-container>
         <v-row justify="center" class="mb-3">
-          <v-col
-            v-for="course in searchResults"
-            :key="course.courseCode"
-            cols="4"
+          <v-col v-for="course in searchResults" :key="course.courseCode" cols="4"
             ><v-card class="mx-auto" max-width="400">
               <v-card-text class="mb-0 pb-0">
                 <div class="text-subtitle-2 mb-1 primary--text">
-                  <v-icon left color="primary" small>
-                    mdi-bookmark-multiple
-                  </v-icon>
+                  <v-icon left color="primary" small> mdi-bookmark-multiple </v-icon>
                   <span>{{ course.courseCode }}</span>
                 </div>
                 <div class="text-subtitle-1 font-weight-medium">
@@ -93,23 +78,21 @@
                 </div>
               </v-card-text>
 
-              <v-card-text
-                class="pt-1 pb-2 d-flex justify-space-between text-body-2"
-              >
+              <v-card-text class="pt-1 pb-2 d-flex justify-space-between text-body-2">
                 <div>
-                  <span>Units:{{ " " }}</span>
+                  <span>Units:{{ ' ' }}</span>
                   <span>{{ course.units }}</span>
                 </div>
                 <div>
-                  <span>Tutorials:{{ " " }}</span>
+                  <span>Tutorials:{{ ' ' }}</span>
                   <span>{{ course.tutorials }}</span>
                 </div>
                 <div>
-                  <span>Lectures:{{ " " }}</span>
+                  <span>Lectures:{{ ' ' }}</span>
                   <span>{{ course.lectures }}</span>
                 </div>
                 <div>
-                  <span>Labs:{{ " " }}</span>
+                  <span>Labs:{{ ' ' }}</span>
                   <span>{{ course.labs }}</span>
                 </div>
               </v-card-text>
@@ -128,7 +111,7 @@
                   >
                     <v-icon left v-if="course.added">mdi-marker-check</v-icon>
                     <v-icon left v-else>mdi-plus</v-icon>
-                    {{ course.added ? "course added" : "add course" }}
+                    {{ course.added ? 'course added' : 'add course' }}
                   </v-btn>
                 </v-row>
               </v-card-actions>
@@ -141,18 +124,18 @@
 </template>
 
 <script>
-import _ from "lodash";
+import _ from 'lodash';
 export default {
-  name: "search-courses",
+  name: 'search-courses',
   data: () => ({
-    typedText: "",
-    searchIcon: "mdi-magnify",
-    clearIcon: "mdi-close",
-    hintText: "",
+    typedText: '',
+    searchIcon: 'mdi-magnify',
+    clearIcon: 'mdi-close',
+    hintText: '',
     searching: false,
     searchResults: null,
     skeletonAttrs: {
-      class: "mb-1",
+      class: 'mb-1',
       boilerplate: false,
       elevation: 2,
     },
@@ -165,24 +148,20 @@ export default {
   }),
   methods: {
     showHint() {
-      this.hintText =
-        "Search by Course Name (eg. Digital Design) or Course Code (eg. EEE F221)";
+      this.hintText = 'Search by Course Name (eg. Digital Design) or Course Code (eg. EEE F221)';
     },
     hideHint() {
-      this.hintText = "";
+      this.hintText = '';
     },
 
-    async fetchCourses(searchText = "", page = 1) {
+    async fetchCourses(searchText = '', page = 1) {
       try {
         this.searching = true;
-        const courses = await this.$store.dispatch("sendRequest", {
-          method: "GET",
+        const courses = await this.$store.dispatch('sendRequest', {
+          method: 'GET',
           url: `courses?search=${searchText}&page=${page}`,
         });
-        this.searchResults = await this.$store.dispatch(
-          "filterSearchResults",
-          courses.data
-        );
+        this.searchResults = await this.$store.dispatch('filterSearchResults', courses.data);
         this.pagination = courses.metadata;
         this.searching = false;
       } catch (err) {
@@ -191,17 +170,12 @@ export default {
     },
 
     async paginationClicked(currentPage) {
-      await this.fetchCourses(this.typedText || "", currentPage);
+      await this.fetchCourses(this.typedText || '', currentPage);
     },
 
     async addCourse(courseID) {
-      const newCourse = this.searchResults.find(
-        (course) => course.courseCode === courseID
-      );
-      const courseAdded = await this.$store.dispatch(
-        "addCourseToUserStore",
-        newCourse
-      );
+      const newCourse = this.searchResults.find((course) => course.courseCode === courseID);
+      const courseAdded = await this.$store.dispatch('addCourseToUserStore', newCourse);
       if (courseAdded)
         this.searchResults = this.searchResults.map((course) =>
           course.courseCode == courseID ? { ...newCourse, added: true } : course
--- frontend-desktop/src/store/api/localDB.js
+++ frontend-desktop/src/store/api/localDB.js
@@ -1,62 +1,62 @@
 import { openDB } from 'idb';
-const DB_VERSION = 6;
+const DB_VERSION = 7;
 const DB_NAME = 'TIMETABLE_BITS_PILANI_VERSION_' + DB_VERSION;
 const DB_STORES = ['USER_COURSES', 'SCHEDULED_COURSES'];
 
 export default {
-	async getDB() {
-		return openDB(DB_NAME, DB_VERSION, {
-			upgrade(db) {
-				DB_STORES.forEach((store) => {
-					if (!db.objectStoreNames.contains(store)) {
-						db.createObjectStore(store, {
-							keyPath: 'courseCode',
-							autoIncrement: true,
-						});
-					}
-				});
-			},
-		});
-	},
+  async getDB() {
+    return openDB(DB_NAME, DB_VERSION, {
+      upgrade(db) {
+        DB_STORES.forEach((store) => {
+          if (!db.objectStoreNames.contains(store)) {
+            db.createObjectStore(store, {
+              keyPath: 'courseCode',
+              autoIncrement: true,
+            });
+          }
+        });
+      },
+    });
+  },
 
-	async writeData(storeName, data) {
-		const db = await this.getDB();
-		const tx = db.transaction(storeName, 'readwrite');
-		const store = tx.objectStore(storeName);
-		store.put(data);
-		return tx.done;
-	},
+  async writeData(storeName, data) {
+    const db = await this.getDB();
+    const tx = db.transaction(storeName, 'readwrite');
+    const store = tx.objectStore(storeName);
+    store.put(data);
+    return tx.done;
+  },
 
-	async writeBulkData(storeName, dataArray) {
-		const db = await this.getDB();
-		const tx = db.transaction(storeName, 'readwrite');
-		const store = tx.objectStore(storeName);
-		let promises = [];
-		dataArray.forEach((data) => promises.push(store.put(data)));
-		promises.push(tx.done);
-		return await Promise.all(promises);
-	},
+  async writeBulkData(storeName, dataArray) {
+    const db = await this.getDB();
+    const tx = db.transaction(storeName, 'readwrite');
+    const store = tx.objectStore(storeName);
+    let promises = [];
+    dataArray.forEach((data) => promises.push(store.put(data)));
+    promises.push(tx.done);
+    return await Promise.all(promises);
+  },
 
-	async readData(storeName) {
-		const db = await this.getDB();
-		const store = db.transaction(storeName, 'readonly').objectStore(storeName);
-		return store.getAll();
-	},
+  async readData(storeName) {
+    const db = await this.getDB();
+    const store = db.transaction(storeName, 'readonly').objectStore(storeName);
+    return store.getAll();
+  },
 
-	async removeItemFromStore(storeName, key) {
-		const db = await this.getDB();
-		const tx = db.transaction(storeName, 'readwrite');
-		const store = tx.objectStore(storeName);
-		store.delete(key);
-		return tx.done;
-	},
+  async removeItemFromStore(storeName, key) {
+    const db = await this.getDB();
+    const tx = db.transaction(storeName, 'readwrite');
+    const store = tx.objectStore(storeName);
+    store.delete(key);
+    return tx.done;
+  },
 
-	async deleteOldDbs() {
-		const dbs = await window.indexedDB.databases();
-		dbs.forEach((db) => {
-			if (db.name !== DB_NAME) {
-				window.indexedDB.deleteDatabase(db.name);
-			}
-		});
-	},
+  async deleteOldDbs() {
+    const dbs = await window.indexedDB.databases();
+    dbs.forEach((db) => {
+      if (db.name !== DB_NAME) {
+        window.indexedDB.deleteDatabase(db.name);
+      }
+    });
+  },
 };
