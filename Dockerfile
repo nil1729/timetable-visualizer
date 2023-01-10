8171444d8e98debccff068be43100542710991c4
NILANJAN DEB <nilanjan172nsvian@gmail.com>
2023-01-10 16:06:51 +0530

    update docker image for pdf

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,23 @@
 FROM node:14.21.1-alpine
 
+# Installs latest Chromium (100) package.
+RUN apk add --no-cache \
+    chromium \
+    nss \
+    freetype \
+    harfbuzz \
+    ca-certificates \
+    ttf-freefont 
+
+# Tell Puppeteer to skip installing Chrome. We'll be using the installed package.
+ENV PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser
+
+# Add user so we don't need --no-sandbox.
+RUN addgroup -S pptruser && adduser -S -G pptruser pptruser \
+    && mkdir -p /home/pptruser/Downloads /app \
+    && chown -R pptruser:pptruser /home/pptruser \
+    && chown -R pptruser:pptruser /app
+
 WORKDIR /app
 
 COPY . /app/
@@ -10,5 +28,8 @@ COPY ./frontend-mobile /app/prod/mobile
 RUN npm run setup_env
 
 RUN npm run prod_setup
+
+# Run everything after as non-privileged user.
+USER pptruser
 
 CMD [ "npm", "run", "start:prod" ]
--- src/routes/pdf-generate/index.js
+++ src/routes/pdf-generate/index.js
@@ -1,42 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const path = require('path');
-const ejs = require('ejs');
-const generateTimetableAPI = require('../../../timetable-generator/generator');
-const generateParsedArrayForPDF = require('../../../timetable-generator/generateParsedArrayForPDF');
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+const ejs = require("ejs");
+const generateTimetableAPI = require("../../../timetable-generator/generator");
+const generateParsedArrayForPDF = require("../../../timetable-generator/generateParsedArrayForPDF");
 
-router.post('/generate-timetable', async (req, res) => {
-	try {
-		let timetableArray = generateParsedArrayForPDF(generateTimetableAPI(req.body));
-		const browser = await puppeteer.launch({
-			headless: true,
-			args: ['--no-sandbox'],
-		});
-		const page = await browser.newPage();
-		const pageHTML = setUpTimetableTemplate(timetableArray);
-		await page.setContent(pageHTML);
+router.post("/generate-timetable", async (req, res) => {
+  try {
+    let timetableArray = generateParsedArrayForPDF(generateTimetableAPI(req.body));
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox"]
+    });
+    const page = await browser.newPage();
+    const pageHTML = setUpTimetableTemplate(timetableArray);
+    await page.setContent(pageHTML);
 
-		const pdfBuffer = await page.pdf({ format: 'A4', landscape: true, timeout: 5 * 60 * 1000 });
+    const pdfBuffer = await page.pdf({ format: "A4", landscape: true, timeout: 5 * 60 * 1000 });
 
-		await page.close();
-		await browser.close();
+    await page.close();
+    await browser.close();
 
-		res.set('Content-Type', 'application/pdf');
-		res.send(pdfBuffer);
-	} catch (error) {
-		console.log(error);
-		return res.status(400).json({ error: error.message });
-	}
+    res.set("Content-Type", "application/pdf");
+    res.send(pdfBuffer);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: error.message });
+  }
 });
 
 const setUpTimetableTemplate = (timetableArray) => {
-	const templateHTML = fs.readFileSync(path.join(__dirname, 'timetable.ejs'), 'utf8');
-	const mainHTML = ejs.render(templateHTML, {
-		timetableArray,
-	});
-	return mainHTML;
+  const templateHTML = fs.readFileSync(path.join(__dirname, "timetable.ejs"), "utf8");
+  const mainHTML = ejs.render(templateHTML, {
+    timetableArray
+  });
+  return mainHTML;
 };
 
 module.exports = router;
